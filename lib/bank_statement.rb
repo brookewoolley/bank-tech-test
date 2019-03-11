03c81ca771eda7906f96a534b6435227f949289e
Brooke Woolley <brooke.woolley@gmail.com>
2019-03-11 12:54:35 +0000

    feat: withdraw method updates the balance

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -5,4 +5,12 @@ class BankStatement
   def initialize(balance = 0)
     @balance = balance
   end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
 end
--- spec/unit/bank_statement_spec.rb
+++ spec/unit/bank_statement_spec.rb
@@ -13,4 +13,12 @@ RSpec.describe BankStatement do
       expect(subject.balance).to eq(1000)
     end
   end
+
+  describe '#withdraw' do
+    it 'can update the balance' do
+      subject.deposit(1000)
+      subject.withdraw(500)
+      expect(subject.balance).to eq(500)
+    end
+  end
 end
