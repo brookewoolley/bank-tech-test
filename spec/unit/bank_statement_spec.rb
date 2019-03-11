f2a6253f3a80f83d5501286cf1b913cb84c0eafe
Brooke Woolley <brooke.woolley@gmail.com>
2019-03-11 12:51:30 +0000

    feat: deposit method updates balance

--- spec/unit/bank_statement_spec.rb
+++ spec/unit/bank_statement_spec.rb
@@ -3,8 +3,14 @@ require 'bank_statement'
 RSpec.describe BankStatement do
   describe '#balance' do
       it 'can show the total' do
-      statement = BankStatement.new
-      expect(statement.balance).to eq(0)
+      expect(subject.balance).to eq(0)
       end
+  end
+
+  describe '#deposit' do
+    it 'can update the balance' do
+      subject.deposit(1000)
+      expect(subject.balance).to eq(1000)
+    end
   end
 end
