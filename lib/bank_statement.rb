ce2fc5f5b2b768e8df8eb97bc979d6e111553c56
Brooke Woolley <brooke.woolley@gmail.com>
2019-03-13 09:30:43 +0000

    feat: printing statement

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -1,5 +1,14 @@
+require_relative 'bank_account'
+
 class BankStatement
   def header
     'date || credit || debit || balance'
+  end
+
+  def print(account)
+    puts header
+    account.transactions.each { |a, b, c, d|
+    puts a + "  ||  " + b.to_s + "  ||  " + c.to_s + "  ||  " + d.to_s
+    }
   end
 end
--- spec/unit/bank_statement_spec.rb
+++ spec/unit/bank_statement_spec.rb
@@ -1,9 +1,14 @@
 require 'bank_statement'
+require 'bank_account'
 
 RSpec.describe BankStatement do
   describe 'print' do
-    it 'can print the header' do
-      expect(subject.header).to eq('date || credit || debit || balance')
+
+    let(:account) { instance_double('Account', transactions: [["12/03/2019", "100.00", nil, "100.00"]]) }
+
+    it 'can print the transactions' do
+      statement = BankStatement.new
+      expect { statement.print(account) }.to output("date || credit || debit || balance\n12/03/2019  ||  100.00  ||    ||  100.00\n").to_stdout
     end
   end
 end
