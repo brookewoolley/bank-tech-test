02b2e62890cbee065069cfb71e4b1594ea3bcef2
Brooke Woolley <brooke.woolley@gmail.com>
2019-03-13 21:29:03 +0000

    tests refactored

--- spec/feature/bank_feature_spec.rb
+++ spec/feature/bank_feature_spec.rb
@@ -2,28 +2,15 @@ require 'bank_account'
 require 'bank_statement'
 
 RSpec.describe BankAccount do
-
-    describe '#deposit' do
-      it 'can update the balance and take date argument' do
-        expect { subject.deposit(1000, '10-01-2017') }.to change { subject.balance }.by(1000)
-      end
-
-      it 'can update the balance without date argument' do
-        expect { subject.deposit(1000) }.to change { subject.balance }.by(1000)
-      end
-
-    end
-
-    describe '#withdraw' do
-      it 'can update the balance and take date argument' do
-        subject.deposit(1000)
-        expect { subject.withdraw(500, '11-03-2017') }.to change { subject.balance }.by(-500)
-      end
-
-      it 'can update the balance without date argument' do
-        subject.deposit(1000)
-        subject.withdraw(500)
-        expect { subject.withdraw(500) }.to change { subject.balance }.by(-500)
-      end
+  it 'can deposit, withdraw and print a statement' do
+    statement = BankStatement.new
+    account = BankAccount.new
+    account.deposit(1000, "08-03-2019")
+    account.withdraw(80, "09-03-2019")
+    account.deposit(20)
+    account.withdraw(10)
+    expect { statement.print(account) }.to output(
+      "date || credit || debit || balance\n13/03/2019  ||    ||  10.00  ||  930.00\n13/03/2019  ||  20.00  ||    ||  940.00\n09/03/2019  ||    ||  80.00  ||  920.00\n08/03/2019  ||  1000.00  ||    ||  1000.00\n"
+    ).to_stdout
   end
 end
--- spec/unit/bank_account_spec.rb
+++ spec/unit/bank_account_spec.rb
@@ -2,7 +2,29 @@ require 'bank_account'
 
 RSpec.describe BankAccount do
 
+  describe '#deposit' do
+    it 'can update the balance and take date argument' do
+      expect { subject.deposit(1000, '10-01-2017') }.to change { subject.balance }.by(1000)
+    end
+
+    it 'can update the balance without date argument' do
+      expect { subject.deposit(1000) }.to change { subject.balance }.by(1000)
+    end
+
+  end
+
   describe '#withdraw' do
+    it 'can update the balance and take date argument' do
+      subject.deposit(1000)
+      expect { subject.withdraw(500, '11-03-2017') }.to change { subject.balance }.by(-500)
+    end
+
+    it 'can update the balance without date argument' do
+      subject.deposit(1000)
+      subject.withdraw(500)
+      expect { subject.withdraw(500) }.to change { subject.balance }.by(-500)
+    end
+
     it 'can check for sufficient funds' do
       expect { subject.withdraw(20) }.to raise_error 'Insufficient funds'
     end
